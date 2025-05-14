dcd46291e2d312ecc95950b15b1beb5c7ddab506
John Cez Casupanan <casupananjohncez@gmail.com>
2025-05-14 23:14:41 +0800

    build: Initialize Vue 3 project with Vite, including basic structure, configuration files, and example components.

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { defineAsyncComponent } from 'vue';
+
+const customerList = defineAsyncComponent(() => import('../components/customerList.vue'));
+</script>
+
+<template>
+    <h1>Home</h1>
+    <customerList />
+</template>
+
+<style scoped>
+
+</style> 
